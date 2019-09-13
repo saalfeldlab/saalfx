6566910cb1c3310cba6e47ecb3582ac11fd66a29
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-09-13 09:28:33 -0400

    Better formatting in Separators.kt

--- src/main/kotlin/org/janelia/saalfeldlab/fx/Separators.kt
+++ src/main/kotlin/org/janelia/saalfeldlab/fx/Separators.kt
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -36,14 +36,10 @@ class Separators {
     companion object {
 
         @JvmStatic
-        fun vertical(): Separator {
-            return withOrientation(Orientation.VERTICAL)
-        }
+        fun vertical(): Separator = withOrientation(Orientation.VERTICAL)
 
         @JvmStatic
-        fun horizontal(): Separator {
-            return withOrientation(Orientation.HORIZONTAL)
-        }
+        fun horizontal(): Separator = withOrientation(Orientation.HORIZONTAL)
 
         @JvmStatic
         fun withOrientation(orientation: Orientation): Separator {
