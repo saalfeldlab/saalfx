3f73096f05b2111f7d10175edc0517961ee93045
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-09-13 11:33:41 -0400

    Add first test for ResizeOnLeftSide

--- src/main/kotlin/org/janelia/saalfeldlab/fx/ui/ResizeOnLeftSide.kt
+++ src/main/kotlin/org/janelia/saalfeldlab/fx/ui/ResizeOnLeftSide.kt
@@ -31,17 +31,17 @@ package org.janelia.saalfeldlab.fx.ui
 import java.util.Optional
 import java.util.function.DoublePredicate
 
-import javafx.beans.property.BooleanProperty
 import javafx.beans.property.DoubleProperty
 import javafx.beans.property.SimpleBooleanProperty
 import javafx.event.EventHandler
-import javafx.geometry.Bounds
 import javafx.scene.Cursor
 import javafx.scene.Node
 import javafx.scene.input.MouseEvent
 import org.janelia.saalfeldlab.fx.event.MouseDragFX
 import java.util.function.Predicate
 import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 class ResizeOnLeftSide @JvmOverloads constructor(
         private val node: Node,
@@ -51,7 +51,10 @@ class ResizeOnLeftSide @JvmOverloads constructor(
         private val maxWidth: Double = 500.0,
         private val isWithinMarginOfBorder: DoublePredicate = DoublePredicate { abs(it) < 5 }) {
 
-    private val isCurrentlyWithinMarginOfBorder = SimpleBooleanProperty(false)
+    private val _isCurrentlyWithinMarginOfBorder = SimpleBooleanProperty(false)
+
+    val isCurrentlyWithinMarginOfBorder: Boolean
+        get() = _isCurrentlyWithinMarginOfBorder.value
 
     private val mouseMoved = MouseMoved()
 
@@ -61,7 +64,7 @@ class ResizeOnLeftSide @JvmOverloads constructor(
 
         this.mouseDragged = object : MouseDragFX(
                 "resize",
-                Predicate { isCurrentlyWithinMarginOfBorder.get() },
+                Predicate { _isCurrentlyWithinMarginOfBorder.get() },
                 true,
                 this,
                 false) {
@@ -72,9 +75,7 @@ class ResizeOnLeftSide @JvmOverloads constructor(
             override fun drag(event: MouseEvent) {
                 val bounds = node.localToScene(node.boundsInLocal)
                 val dx = event.sceneX - bounds.minX
-                this@ResizeOnLeftSide.width.set(Math.min(
-                        Math.max(width.get() - dx, this@ResizeOnLeftSide.minWidth),
-                        this@ResizeOnLeftSide.maxWidth))
+                this@ResizeOnLeftSide.width.set(min(max(width.get() - dx, this@ResizeOnLeftSide.minWidth), this@ResizeOnLeftSide.maxWidth))
             }
 
             override fun endDrag(event: MouseEvent) {
@@ -82,7 +83,7 @@ class ResizeOnLeftSide @JvmOverloads constructor(
             }
         }
 
-        isCurrentlyWithinMarginOfBorder.addListener { obs, oldv, newv ->
+        _isCurrentlyWithinMarginOfBorder.addListener { obs, oldv, newv ->
             if (!mouseDragged.isDraggingProperty.get()) {
                 Optional.ofNullable(node.scene).ifPresent { s ->
                     s.cursor = if (newv)
@@ -102,7 +103,7 @@ class ResizeOnLeftSide @JvmOverloads constructor(
     fun remove() {
         node.parent.removeEventFilter(MouseEvent.MOUSE_MOVED, mouseMoved)
         this.mouseDragged.removeFromAsFilter(node.parent)
-        this.isCurrentlyWithinMarginOfBorder.set(false)
+        this._isCurrentlyWithinMarginOfBorder.set(false)
         this.mouseDragged.abortDrag()
     }
 
@@ -111,7 +112,7 @@ class ResizeOnLeftSide @JvmOverloads constructor(
         override fun handle(event: MouseEvent) {
             val bounds = node.boundsInParent
             val y = event.y
-            isCurrentlyWithinMarginOfBorder.set(y >= bounds.minY && y <= bounds.maxY && isWithinMarginOfBorder.test(event.x - bounds.minX))
+            _isCurrentlyWithinMarginOfBorder.set(y >= bounds.minY && y <= bounds.maxY && isWithinMarginOfBorder.test(event.x - bounds.minX))
         }
     }
 
---
+++ src/test/kotlin/org/janelia/saalfeldlab/fx/ui/ResizeOnLeftSideTest.kt
@@ -0,0 +1,93 @@
+package org.janelia.saalfeldlab.fx.ui
+
+import javafx.application.Platform
+import javafx.event.Event
+import javafx.geometry.Point2D
+import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.control.TextArea
+import javafx.scene.input.MouseEvent
+import javafx.scene.layout.BorderPane
+import javafx.scene.layout.Region
+import javafx.scene.layout.StackPane
+import javafx.stage.Stage
+import org.junit.Assert
+import org.junit.Test
+import org.slf4j.LoggerFactory
+import org.testfx.api.FxRobot
+import org.testfx.framework.junit.ApplicationTest
+import java.lang.invoke.MethodHandles
+import java.util.function.DoublePredicate
+import kotlin.math.abs
+
+class ResizeOnLeftSideTest : ApplicationTest() {
+
+    private lateinit var node: Region
+
+    private lateinit var resizer: ResizeOnLeftSide
+
+    override fun start(stage: Stage) {
+
+        node = TextArea("Node!")
+                .also { it.prefWidth = START_WIDTH }
+                .also { it.addEventFilter(Event.ANY) { LOG.trace("Filtering event in node: {}", it) } }
+
+        resizer = ResizeOnLeftSide(
+                node,
+                node.prefWidthProperty(),
+                isWithinMarginOfBorder = IS_WITHIN_MARGIN_OF_BORDER)
+
+        val center = TextArea("Center!")
+                .also { it.addEventFilter(Event.ANY) { LOG.trace("Filtering event in center: {}", it) } }
+        val pane = BorderPane(center)
+                .also { it.right = node }
+        resizer.install()
+        stage.scene = Scene(pane, SCENE_WIDTH, SCENE_HEIGHT)
+                .also { it.addEventFilter(Event.ANY) { LOG.trace("Filtering event in scene: {}", it) } }
+                .also { it.addEventFilter(MouseEvent.ANY) { LOG.trace("Filtering mouse event in scene: {}", it) } }
+        stage.show()
+        // This is necessary to make sure that the stage grabs focus from OS and events are registered
+        // https://stackoverflow.com/a/47685356/1725687
+        Platform.runLater { stage.isIconified = true; stage.isIconified = false }
+    }
+
+    @Test
+    fun testInitialState() {
+        Assert.assertEquals(START_WIDTH, node.prefWidth, 0.0)
+        Assert.assertEquals(node.boundsInParent.minX, SCENE_WIDTH - START_WIDTH, 0.0)
+    }
+
+    @Test
+    fun testIsWithinMargin() {
+        val offsetsAndResizable = arrayOf(
+                Pair(-RESIZABLE_DISTANE - 1,   false),
+                Pair(-RESIZABLE_DISTANE,       false), // IS_WITHIN_MARGIN_OF_BORDER requires strictly smaller
+                Pair(-RESIZABLE_DISTANE / 2.0, true),
+                Pair( 0.0,                     true),
+                Pair( RESIZABLE_DISTANE / 2.0, true),
+                Pair( RESIZABLE_DISTANE,       false), // IS_WITHIN_MARGIN_OF_BORDER requires strictly smaller
+                Pair( RESIZABLE_DISTANE + 1,   false))
+        offsetsAndResizable.forEach { (offset, resizable) ->
+            LOG.debug("Moving mouse to offset=$offset relative to left border of right node")
+            moveTo(node, Point2D(-node.width / 2.0 + offset, 0.0))
+            LOG.debug("{} {} {} {}", offset, resizable, resizer.isCurrentlyWithinMarginOfBorder)
+            Assert.assertEquals(resizable, resizer.isCurrentlyWithinMarginOfBorder)
+        }
+    }
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass())
+
+        const val START_WIDTH = 100.0
+
+        const val SCENE_WIDTH = 800.0
+
+        const val SCENE_HEIGHT = 600.0
+
+        const val RESIZABLE_DISTANE = 5.0
+
+        val IS_WITHIN_MARGIN_OF_BORDER = DoublePredicate { abs(it) < RESIZABLE_DISTANE }
+
+    }
+
+}
