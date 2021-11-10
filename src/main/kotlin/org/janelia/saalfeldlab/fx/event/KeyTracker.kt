7c81b66a15f2350a06d1d3ca1ca16c63ed6505d0
Caleb Hulbert <cmhulbert@users.noreply.github.com>
2021-11-10 09:19:12 -0500

    refactor!: change to `InstallAndRemove<Window>`
    Effectively, it was already InstallAndRemove<Window>.
    Remove deprecated methods.
    Add some private convenience methods for style

--- src/main/kotlin/org/janelia/saalfeldlab/fx/event/KeyTracker.kt
+++ src/main/kotlin/org/janelia/saalfeldlab/fx/event/KeyTracker.kt
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -29,105 +29,51 @@
 package org.janelia.saalfeldlab.fx.event
 
 import javafx.beans.value.ChangeListener
-import javafx.beans.value.ObservableValue
 import javafx.event.EventHandler
 import javafx.scene.Scene
 import javafx.scene.input.KeyCode
 import javafx.scene.input.KeyEvent
 import javafx.stage.Window
-import org.janelia.saalfeldlab.fx.util.OnWindowInitListener
-import java.util.function.Consumer
 
-class KeyTracker : InstallAndRemove<Scene> {
+class KeyTracker : InstallAndRemove<Window> {
 
     private val activeKeys = mutableSetOf<KeyCode>()
 
-    private val activate = ActivateKey()
+    private val activateKeyHandler = EventHandler<KeyEvent> { activeKeys.synchronized { add(it.code) } }
 
-    private val deactivate = DeactivateKey()
+    private val deactivateKeyHandler = EventHandler<KeyEvent> { activeKeys.synchronized { remove(it.code) } }
 
-    private val onFocusChanged = OnFocusChanged()
+    private val onFocusChanged = ChangeListener<Boolean?> { _, _, new -> new?.let { activeKeys.synchronized { clear() } } }
 
-    fun installInto(window: Window) = installInto(window.scene, window)
+    override fun installInto(t: Window) = installInto(t.scene, t)
+    override fun removeFrom(t: Window) = removeFrom(t.scene, t)
 
     fun installInto(scene: Scene, window: Window) {
-        scene.addEventFilter(KeyEvent.KEY_RELEASED, deactivate)
-        scene.addEventFilter(KeyEvent.KEY_PRESSED, activate)
+        scene.addEventFilter(KeyEvent.KEY_RELEASED, deactivateKeyHandler)
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, activateKeyHandler)
         window.focusedProperty().addListener(onFocusChanged)
     }
 
     fun removeFrom(scene: Scene, window: Window) {
-        scene.removeEventFilter(KeyEvent.KEY_RELEASED, deactivate)
-        scene.removeEventFilter(KeyEvent.KEY_PRESSED, activate)
+        scene.removeEventFilter(KeyEvent.KEY_RELEASED, deactivateKeyHandler)
+        scene.removeEventFilter(KeyEvent.KEY_PRESSED, activateKeyHandler)
         window.focusedProperty().removeListener(onFocusChanged)
     }
 
-    @Deprecated("Install into scene and window directly instead", ReplaceWith("installInto(scene, window)"))
-    override fun installInto(t: Scene) {
-        t.addEventFilter(KeyEvent.KEY_RELEASED, deactivate)
-        t.addEventFilter(KeyEvent.KEY_PRESSED, activate)
-        t.windowProperty().addListener(OnWindowInitListener(Consumer { window -> window.focusedProperty().addListener(onFocusChanged) }))
-    }
+    fun areOnlyTheseKeysDown(vararg codes: KeyCode) = activeKeys.synchronized { mutableSetOf(*codes) == this }
 
-    @Deprecated("Remove from scene and window directly instead", ReplaceWith("removeFrom(scene, window)"))
-    override fun removeFrom(t: Scene) {
-        t.removeEventFilter(KeyEvent.KEY_PRESSED, activate)
-        t.removeEventFilter(KeyEvent.KEY_RELEASED, deactivate)
-        t.window?.focusedProperty()?.removeListener(onFocusChanged)
-    }
+    fun areKeysDown(vararg codes: KeyCode) = activeKeys.synchronized { containsAll(listOf(*codes)) }
 
-    private inner class ActivateKey : EventHandler<KeyEvent> {
-        override fun handle(event: KeyEvent) {
-            synchronized(activeKeys) {
-                activeKeys.add(event.code)
-            }
-        }
-    }
+    fun activeKeyCount() = activeKeys.synchronized { size }
 
-    private inner class DeactivateKey : EventHandler<KeyEvent> {
-        override fun handle(event: KeyEvent) {
-            synchronized(activeKeys) {
-                activeKeys.remove(event.code)
-            }
-        }
-    }
-
-    private inner class OnFocusChanged : ChangeListener<Boolean> {
-
-        override fun changed(observable: ObservableValue<out Boolean>, oldValue: Boolean?, newValue: Boolean?) {
-            newValue?.let {
-                if (!newValue)
-                    synchronized(activeKeys) {
-                        activeKeys.clear()
-                    }
-            }
-        }
-
-    }
-
-    fun areOnlyTheseKeysDown(vararg codes: KeyCode): Boolean {
-        val codesHashSet = mutableSetOf(*codes)
-        synchronized(activeKeys) {
-            return codesHashSet == activeKeys
-        }
-    }
-
-    fun areKeysDown(vararg codes: KeyCode): Boolean {
-        synchronized(activeKeys) {
-            return activeKeys.containsAll(listOf(*codes))
-        }
-    }
-
-    fun activeKeyCount(): Int {
-        synchronized(activeKeys) {
-            return activeKeys.size
+    private fun <R> MutableSet<KeyCode>.synchronized(run: MutableSet<KeyCode>.() -> R): R {
+        synchronized(this) {
+            return run(this)
         }
     }
 
     fun noKeysActive() = activeKeyCount() == 0
 
-    fun getActiveKeyCodes(includeModifiers: Boolean) = synchronized(activeKeys) {
-        activeKeys.filter { includeModifiers || !it.isModifierKey }
-    }
+    fun getActiveKeyCodes(includeModifiers: Boolean) = activeKeys.synchronized { filter { includeModifiers || !it.isModifierKey } }
 
 }
