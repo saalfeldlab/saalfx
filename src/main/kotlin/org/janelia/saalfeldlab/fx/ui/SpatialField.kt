580b4b07927ee6a11613444e897d7f9851ccaac8
Caleb Hulbert <cmhulbert@users.noreply.github.com>
2021-11-10 09:16:56 -0500

    refactor!: simplified methods to get typed arrays
    previous `getAs` calls assumed a size 3 array anyway, so remove the param for the array (so we can gaurantee the right size).
    
    Also removed the separate kotlin and java variants.

--- src/main/kotlin/org/janelia/saalfeldlab/fx/ui/SpatialField.kt
+++ src/main/kotlin/org/janelia/saalfeldlab/fx/ui/SpatialField.kt
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -28,19 +28,15 @@
  */
 package org.janelia.saalfeldlab.fx.ui
 
-import java.util.function.DoublePredicate
-import java.util.function.IntPredicate
-import java.util.function.LongPredicate
-
 import javafx.beans.property.DoubleProperty
 import javafx.beans.property.IntegerProperty
 import javafx.beans.property.LongProperty
 import javafx.beans.property.Property
 import javafx.scene.Node
 import javafx.scene.layout.HBox
-import java.util.function.ToDoubleFunction
-import java.util.function.ToIntFunction
-import java.util.function.ToLongFunction
+import java.util.function.DoublePredicate
+import java.util.function.IntPredicate
+import java.util.function.LongPredicate
 
 class SpatialField<P : Property<Number>> private constructor(
         val x: NumberField<P>,
@@ -60,44 +56,33 @@ class SpatialField<P : Property<Number>> private constructor(
         this.node = HBox(x.textField, y.textField, z.textField)
     }
 
-    fun getAs(xyz: LongArray, converter: (Number) -> Long = { it.toLong() }) = getAs(xyz, ToLongFunction { converter(it) })
-
-    @JvmOverloads
-    fun getAs(xyz: LongArray, converter: ToLongFunction<Number> = ToLongFunction{  it.toLong() }): LongArray {
-        xyz[0] = x.valueProperty().value.let { converter.applyAsLong(it) }
-        xyz[1] = y.valueProperty().value.let { converter.applyAsLong(it) }
-        xyz[2] = z.valueProperty().value.let { converter.applyAsLong(it) }
-        return xyz
+    fun asLongArray() = LongArray(3).apply {
+        this[0] = x.value.toLong()
+        this[1] = y.value.toLong()
+        this[2] = z.value.toLong()
     }
 
-    fun getAs(xyz: IntArray, converter: (Number) -> Int = { it.toInt() }) = getAs(xyz, ToIntFunction { converter(it) })
-
-    @JvmOverloads
-    fun getAs(xyz: IntArray, converter: ToIntFunction<Number> = ToIntFunction{  it.toInt() }): IntArray {
-        xyz[0] = x.valueProperty().value.let { converter.applyAsInt(it) }
-        xyz[1] = y.valueProperty().value.let { converter.applyAsInt(it) }
-        xyz[2] = z.valueProperty().value.let { converter.applyAsInt(it) }
-        return xyz
+    fun asIntArray() = IntArray(3).apply {
+        this[0] = x.value.toInt()
+        this[1] = y.value.toInt()
+        this[2] = z.value.toInt()
     }
 
-    fun getAs(xyz: DoubleArray, converter: (Number) -> Double = { it.toDouble() }) = getAs(xyz, ToDoubleFunction { converter(it) })
-
-    @JvmOverloads
-    fun getAs(xyz: DoubleArray, converter: ToDoubleFunction<Number> = ToDoubleFunction{  it.toDouble() }): DoubleArray {
-        xyz[0] = x.valueProperty().value.let { converter.applyAsDouble(it) }
-        xyz[1] = y.valueProperty().value.let { converter.applyAsDouble(it) }
-        xyz[2] = z.valueProperty().value.let { converter.applyAsDouble(it) }
-        return xyz
+    fun asDoubleArray() = DoubleArray(3).apply {
+        this[0] = x.value.toDouble()
+        this[1] = y.value.toDouble()
+        this[2] = z.value.toDouble()
     }
 
     companion object {
 
         @JvmStatic
         fun doubleField(
-                initialValue: Double,
-                test: DoublePredicate,
-                textFieldWidth: Double,
-                vararg submitOn: ObjectField.SubmitOn): SpatialField<DoubleProperty> {
+            initialValue: Double,
+            test: DoublePredicate,
+            textFieldWidth: Double,
+            vararg submitOn: ObjectField.SubmitOn
+        ): SpatialField<DoubleProperty> {
             return SpatialField(
                     NumberField.doubleField(initialValue, test, *submitOn),
                     NumberField.doubleField(initialValue, test, *submitOn),
