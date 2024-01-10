827913125e57eb515c32de26ba2b65a391bc587d
Caleb Hulbert <cmhulbert@gmail.com>
2024-01-10 16:37:37 -0500

    feat: support modifier only keybindings

--- src/main/kotlin/org/janelia/saalfeldlab/fx/actions/NamedKeyCombination.kt
+++ src/main/kotlin/org/janelia/saalfeldlab/fx/actions/NamedKeyCombination.kt
@@ -1,25 +1,47 @@
 package org.janelia.saalfeldlab.fx.actions
 
+import com.sun.javafx.tk.Toolkit
 import javafx.beans.property.SimpleObjectProperty
-import javafx.scene.input.KeyCode
-import javafx.scene.input.KeyCodeCombination
-import javafx.scene.input.KeyCombination
-import javafx.scene.input.KeyEvent
+import javafx.scene.input.*
 import org.apache.commons.lang.builder.ToStringBuilder
 import org.apache.commons.lang.builder.ToStringStyle
 import org.janelia.saalfeldlab.fx.extensions.nonnull
 import kotlin.collections.set
 
-class NamedKeyCombination(val name: String, primaryCombination: KeyCodeCombination) {
+private val KeyCombination.modifierCodes: Set<KeyCode>
+	get() = setOfNotNull(
+		if (shift == KeyCombination.ModifierValue.DOWN) KeyCode.SHIFT else null,
+		if (control == KeyCombination.ModifierValue.DOWN) KeyCode.CONTROL else null,
+		if (alt == KeyCombination.ModifierValue.DOWN) KeyCode.ALT else null,
+		if (meta == KeyCombination.ModifierValue.DOWN) KeyCode.META else null,
+		if (shortcut == KeyCombination.ModifierValue.DOWN) Toolkit.getToolkit().platformShortcutKey else null
+	)
 
-	constructor(name: String, keyCode: KeyCode, vararg modifiers: KeyCombination.Modifier) : this(name, KeyCodeCombination(keyCode, *modifiers))
+private val KeyEvent.modifierCodes: Set<KeyCode>
+	get() = setOfNotNull(
+		if (isShiftDown) KeyCode.SHIFT else null,
+		if (isControlDown) KeyCode.CONTROL else null,
+		if (isAltDown) KeyCode.ALT else null,
+		if (isMetaDown) KeyCode.META else null,
+		if (isShortcutDown) Toolkit.getToolkit().platformShortcutKey else null
+	)
+
+class NamedKeyCombination(val name: String, primaryCombination: KeyCombination) {
 
 	private val primaryCombinationProperty = SimpleObjectProperty(primaryCombination)
-	var primaryCombination: KeyCodeCombination by primaryCombinationProperty.nonnull()
+	var primaryCombination: KeyCombination by primaryCombinationProperty.nonnull()
 
 	fun primaryCombinationProperty() = primaryCombinationProperty
 
 	fun matches(event: KeyEvent) = primaryCombination.match(event)
+
+	val keyCodes: Set<KeyCode>
+		get() {
+			val codes = mutableSetOf<KeyCode>()
+			(primaryCombination as? KeyCodeCombination)?.code?.also { codes += it }
+			codes += primaryCombination.modifierCodes
+			return codes.toSet()
+		}
 
 	val deepCopy: NamedKeyCombination
 		get() = NamedKeyCombination(name, primaryCombination)
@@ -53,7 +75,16 @@ class NamedKeyCombination(val name: String, primaryCombination: KeyCodeCombinati
 			this[keyCombination.name] = keyCombination
 		}
 
-		fun matches(name: String, event: KeyEvent) = get(name)!!.matches(event)
+		fun matches(name: String, event: KeyEvent, keysExclusive : Boolean = true) : Boolean {
+			val namedCombo = get(name)!!
+			return if (keysExclusive) {
+				namedCombo.matches(event)
+			} else {
+				val combo = namedCombo.primaryCombination
+				val codesMatchIfCodeCombo = (combo as? KeyCodeCombination)?.code?.let { it == event.code } ?: true
+				codesMatchIfCodeCombo && event.modifierCodes.containsAll(combo.modifierCodes)
+			}
+		}
 
 		operator fun plusAssign(keyCombination: NamedKeyCombination) = addCombination(keyCombination)
 
@@ -65,6 +96,12 @@ class NamedKeyCombination(val name: String, primaryCombination: KeyCodeCombinati
 
 		val deepCopy: CombinationMap
 			get() = values.map { it.deepCopy }.toTypedArray().let { CombinationMap(*it) }
+	}
+
+	class OnlyModifierKeyCombination(vararg modifier: Modifier) : KeyCombination(*modifier) {
+		override fun match(event: KeyEvent?): Boolean {
+			return super.match(event)
+		}
 	}
 
 }
